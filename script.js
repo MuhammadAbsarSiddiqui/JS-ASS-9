80e0525fd5ebd8b429dff159b5c7196c629b88bb
MuhammadAbsarSiddiqui <128141992+MuhammadAbsarSiddiqui@users.noreply.github.com>
2024-02-25 19:58:09 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// *****Q1*****
+// let software_company={
+//     name:"Web Wizards",
+//     founder:"ABSAR SIDDIQUI",
+//     yearFounded:2024,
+//     products:["HTML","CSS","BOOTSTRAP","JS"]
+// }
+// console.log("The company name is:",software_company.name);
+// console.log("The founder name is:",software_company.founder);
+
+
+// *****Q2*****
+
+let car={
+    brand:"Honda",
+    model:"Civic",
+    year:2023,
+
+    drive: function()
+    {
+        console.log("The "+car.brand+" "+car.model+" of year " +car.year+" is being driven");
+    }
+}
+car.drive();
